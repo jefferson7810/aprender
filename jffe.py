23d0b644f8b56a3b2eb25a0711925ab254796e79
José Jefferson rodrigues da silva <130522213+jefferson7810@users.noreply.github.com>
2023-06-17 18:00:26 -0300

    Update jffe.py

--- jffe.py
+++ jffe.py
@@ -6,6 +6,11 @@ a = int(input("digite o primeiro número:")
 b = int(input("digite o segundo número:")
 
 multi = "multiplicação", (a * b)
-soma = 
+soma = "soma", ( a+ b)
+
+lista = [mult, soma]
+
+for i in lista
+print(i)
         
         
