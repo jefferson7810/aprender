f40267fea2d31dde6ab31460c19cb38d9a98970c
José Jefferson rodrigues da silva <130522213+jefferson7810@users.noreply.github.com>
2023-06-17 18:13:37 -0300

    Update jffe.py

--- jffe.py
+++ jffe.py
@@ -1,16 +1,15 @@
 i = "ola mundo"
 print(i)
-j = input("primeira entrada de dados por input no github")
-print(j)
-a = int(input("digite o primeiro número:")
-b = int(input("digite o segundo número:")
+
+a = int(input("digite o primeiro número:"))
+b = int(input("digite o segundo número:"))
 
 multi = "multiplicação", (a * b)
 soma = "soma", ( a+ b)
 
-lista = [mult, soma]
+lista = multi, soma
 
-for i in lista
-print(i)
+for i in lista:
+    print(i)
         
         
