1c397a1ee9b48727404ea5f293a6d3269aa8ae46
José Jefferson rodrigues da silva <130522213+jefferson7810@users.noreply.github.com>
2023-06-17 17:58:00 -0300

    Update jffe.py

--- jffe.py
+++ jffe.py
@@ -2,3 +2,10 @@ i = "ola mundo"
 print(i)
 j = input("primeira entrada de dados por input no github")
 print(j)
+a = int(input("digite o primeiro número:")
+b = int(input("digite o segundo número:")
+
+multi = "multiplicação", (a * b)
+soma = 
+        
+        
